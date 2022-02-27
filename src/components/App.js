2adfd57ce85a486f6719b3a148a0ba81dd394da6
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-02-27 12:54:45 +0300

    Общий компонент PopupWithForm

--- src/components/App.js
+++ src/components/App.js
@@ -28,6 +28,7 @@ import trash from '../images/trash.svg';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import PopupWithForm from './PopupWithForm';
 
 function App() {
 return (
@@ -36,44 +37,28 @@ return (
     <Header />
     <Main />
     <Footer />
-    
-    <div className="popup profile-popup">
-      <div className="popup-overlay popup__close"></div>
-      <div className="popup__content">  
-        <button className="popup__button popup__close" type="button">
-        </button>
-        <form className="popup__input popup__input_profile" name="popup__input_profile">
-          <h2 className="popup__input-title">Редактировать профиль</h2>
-          <input type="text" className="popup__input-text popup__input-text_type_name" id="popup-name" name="name" placeholder="Имя" required
-            minlength="2" maxlength="40" /> 
-          <span id="popup-name-error" className="error"></span>
-          <input type="text" className="popup__input-text popup__input-text_type_specialty" id="popup-specialty" name="about" placeholder="Специальность" required
-            minlength="2" maxlength="200" />
-          <span id="popup-specialty-error" className="error"></span>
-          <button type="submit" className="popup__input-button">
-            Сохранить
-          </button>
-        </form>
-      </div> 
-    </div>
-    <div className="popup card-popup">
-      <div className="popup-overlay popup-overlay-card popup__close"></div>
-      <div className="popup__content">  
-        <button className="popup__button popup__button_card popup__close" type="button">
-        </button>
-        <form className="popup__input popup__input_newEl" name="popup__input_newEl">
-          <h2 className="popup__input-title">Новое место</h2>
-          <input type="text" className="popup__input-text popup__input-text_type_title" id="popup-title" name="name" placeholder="Название" required
-            minlength="2" maxlength="30" /> 
-          <span id="popup-title-error" className="error"></span>
-          <input type="url" className="popup__input-text popup__input-text_type_image" id="popup-image" name="link" placeholder="Ссылка" required />
-          <span id="popup-image-error" className="error"></span>
-          <button type="submit" className="popup__input-button popup__input-button_card">
-            Создать
-          </button>
-        </form>
-      </div> 
-    </div>
+    <PopupWithForm name="profile-popup" title="Редактировать профиль" button="Сохранить">
+      <input type="text" className="popup__input-text popup__input-text_type_name" id="popup-name" name="name" placeholder="Имя" required
+        minlength="2" maxlength="40" /> 
+      <span id="popup-name-error" className="error"></span>
+      <input type="text" className="popup__input-text popup__input-text_type_specialty" id="popup-specialty" name="about" placeholder="Специальность" required
+        minlength="2" maxlength="200" />
+      <span id="popup-specialty-error" className="error"></span>
+    </PopupWithForm>
+    <PopupWithForm name="card-popup" title="Новое место" button="Создать">
+      <input type="text" className="popup__input-text popup__input-text_type_title" id="popup-title" name="name" placeholder="Название" required
+        minlength="2" maxlength="30" /> 
+      <span id="popup-title-error" className="error"></span>
+      <input type="url" className="popup__input-text popup__input-text_type_image" id="popup-image" name="link" placeholder="Ссылка" required />
+      <span id="popup-image-error" className="error"></span>
+    </PopupWithForm>
+    <PopupWithForm name="confirm-popup" title="Обновить аватар" button="Да" />
+    <PopupWithForm name="avatar-popup" title="Новое место" button="Сохранить">
+      <input type="url" className="popup__input-text popup__input-text_type_image popup__input-text_type_avatarUrl" id="popup-avatar" name="name" placeholder="Ссылка на аватар" required
+        minlength="2" maxlength="300"/> 
+      <span id="popup-avatar-error" className="error"></span>
+    </PopupWithForm>
+
     <div className="popup popup-image">
       <div className="popup-overlay popup-overlay-image popup__close"></div>
       <div className="popup-image__content">
@@ -84,35 +69,8 @@ return (
       </div>  
     </div>
   </div>
-  <div className="popup confirm-popup">
-    <div className="popup-overlay popup__close"></div>
-    <div className="popup__content">  
-      <button className="popup__button popup__close" type="button">
-      </button>
-      <form className="popup__input popup__input_confirm-delete" name="popup__input_profile">
-        <h2 className="popup__input-title">Вы уверены</h2>
-        <button type="submit" className="popup__input-button popup__input-button_confirm">
-          Да
-        </button>
-      </form> 
-    </div> 
-  </div>  
-  <div className="popup avatar-popup">
-    <div className="popup-overlay popup__close"></div>
-    <div className="popup__content">  
-      <button className="popup__button popup__close" type="button">
-      </button>
-      <form className="popup__input popup__input_profile" name="popup__input_profile">
-        <h2 className="popup__input-title">Обновить аватар</h2>
-        <input type="url" className="popup__input-text popup__input-text_type_image popup__input-text_type_avatarUrl" id="popup-avatar" name="name" placeholder="Ссылка на аватар" required
-          minlength="2" maxlength="300"/> 
-        <span id="popup-avatar-error" className="error"></span>
-        <button type="submit" className="popup__input-button">
-          Сохранить
-        </button>
-      </form>
-    </div> 
-  </div>
+
+  
 <template className="template">
   <article className="elements__item">
     <button className="elements__button-delete" type="button">
